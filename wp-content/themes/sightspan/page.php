72d0b180e12a16e3867f33077c3425fd2da664b2
Fahad <webdevelopment@sightspan.com>
2018-06-25 18:01:35 +0500

    fix

--- wp-content/themes/sightspan/page.php
+++ wp-content/themes/sightspan/page.php
@@ -562,13 +562,13 @@ get_sidebar('topbanner');
             </div>
           </div>
 		
-          <?php for($l=4;$l<11;$l++){
+          <?php for($l=3;$l<11;$l++){
 			
-           if($l==4){
+           if($l==3){
 				$link	=	get_site_url()."/unarmed-armed/";
-			}elseif($l==5){
+			}elseif($l==4){
 				$link	=	get_site_url()."/industrial-physical-security/";
-			}elseif($l==6){
+			}elseif($l==5){
 				$link	=	get_site_url()."/autonomous-website/";
 			}		
 		  
